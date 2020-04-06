76e00e0a2f7049dbb4b090c6f4619fc5d10034bc
moonelysian <dandyblack@naver.com>
2020-04-07 00:39:04 +0900

    [#23] add: section, user 초기 데이터 추가 - section: 해야할일, 하는 중, 다했어 초기 데이터로 추가 - user: 회원가입 없이 구현할꺼라 인증용 유저 한명 만들어줌

---
+++ BE/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO section(id, name) values (1, '해야할일');
+INSERT INTO section(id, name) values (2, '하는 중');
+INSERT INTO section(id, name) values (3, '다했어');
+
+INSERT INTO user(user_id, name) values ('john04','John Smith')
