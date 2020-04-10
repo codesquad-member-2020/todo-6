7bb260a5ef5154241bc2b107dab31ba2ec103f91
beemiel <qnswwrn@gmail.com>
2020-04-10 13:50:37 +0900

    [#49] feat: 전체 목록 mock data 추가

--- BE/src/main/java/com/codesquad/todo/controller/TodoController.java
+++ BE/src/main/java/com/codesquad/todo/controller/TodoController.java
@@ -1,6 +1,9 @@
 package com.codesquad.todo.controller;
 
 import com.codesquad.todo.domain.ApiResponse;
+import com.codesquad.todo.domain.User;
+import com.codesquad.todo.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/todo")
 public class TodoController {
 
+  @Autowired
+  private UserRepository userRepository;
+
   @GetMapping
   public ResponseEntity<ApiResponse> showTodoList() {
-    return null;
+    User user = userRepository.findById(1).get();
+    ApiResponse apiResponse = new ApiResponse("SUCCESS",user);
+    return ResponseEntity.ok(apiResponse);
   }
 }
--- BE/src/main/java/com/codesquad/todo/domain/ApiResponse.java
+++ BE/src/main/java/com/codesquad/todo/domain/ApiResponse.java
@@ -1,5 +1,8 @@
 package com.codesquad.todo.domain;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class ApiResponse {
   private String status;
   private Object data;
