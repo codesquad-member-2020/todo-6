28a0a2a5e92645dede2f07776d357b00e23a6af8
moonelysian <dandyblack@naver.com>
2020-04-10 15:40:49 +0900

    [#27] update: Api 응답에 필요한 getter setter 추가

--- BE/src/main/java/com/codesquad/todo/domain/ApiResponse.java
+++ BE/src/main/java/com/codesquad/todo/domain/ApiResponse.java
@@ -8,4 +8,20 @@ public class ApiResponse {
     this.status = status;
     this.data = data;
   }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public Object getData() {
+    return data;
+  }
+
+  public void setData(Object data) {
+    this.data = data;
+  }
 }
