09d31899faaf54bf93fe567341ff081804d7ebb9
beemiel <qnswwrn@gmail.com>
2020-04-10 15:09:37 +0900

    Configuration 어노테이션 추가

--- BE/src/main/java/com/codesquad/todo/CorsConfig.java
+++ BE/src/main/java/com/codesquad/todo/CorsConfig.java
@@ -1,9 +1,11 @@
 package com.codesquad.todo;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
