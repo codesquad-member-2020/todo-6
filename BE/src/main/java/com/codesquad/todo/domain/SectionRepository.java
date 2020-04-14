24e86dcf857e3b23d41bee71d703decc93037db5
moonelysian <dandyblack@naver.com>
2020-04-14 16:25:41 +0900

    [#29] feat: 카드 삭제 기능 추가 - section repository 사용해서 카드 삭제 - 삭제 activity 추가

--- BE/src/main/java/com/codesquad/todo/domain/SectionRepository.java
+++ BE/src/main/java/com/codesquad/todo/domain/SectionRepository.java
@@ -1,6 +1,17 @@
 package com.codesquad.todo.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface SectionRepository extends CrudRepository<Section, Long> {
+
+  @Query("SELECT * FROM card c LEFT JOIN section s ON c.section = s.id WHERE c.id = :cardId AND s.id = :sectionId")
+  Optional<Card> findCardBySectionIdAndCardId(Long sectionId, Long cardId);
+
+  @Modifying
+  @Query("DELETE c FROM card c LEFT JOIN section s ON c.section = s.id WHERE c.id = :cardId AND s.id = :sectionId")
+  void deleteCard(Long sectionId, Long cardId);
 }
