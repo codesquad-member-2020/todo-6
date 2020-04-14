896cc6afca089a1c467d79708da05f76093fd79e
moonelysian <dandyblack@naver.com>
2020-04-14 18:36:03 +0900

    [#32] user랑 activity join 해서 ActivityDto 반환하는 쿼리 작성 - project에서 하려면 어찌 해야할지 아직 모르겠당 두개 쪼인해야하낭

--- BE/src/main/java/com/codesquad/todo/domain/ActivityRepository.java
+++ BE/src/main/java/com/codesquad/todo/domain/ActivityRepository.java
@@ -1,6 +1,13 @@
 package com.codesquad.todo.domain;
 
+import com.codesquad.todo.dto.ActivityDto;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
+
+  @Query("SELECT a.*, u.user_id FROM activity a LEFT JOIN user u ON a.user = u.id WHERE a.project = 1 ORDER BY a.project_key;")
+  List<ActivityDto> getActivities();
 }
