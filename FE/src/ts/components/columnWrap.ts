b6c4473d12ef2ace9c8bdca95ff57f9ab670c3fe
최성익 <tjddlr92@gmail.com>
2020-04-17 01:45:10 +0900

    [#18] Feat: 컬럼 추가 버튼 스타일링
    - 기능 없이 스타일만 추가

--- FE/src/ts/components/columnWrap.ts
+++ FE/src/ts/components/columnWrap.ts
@@ -1,11 +1,20 @@
 import { _q } from '../utils/utils';
+import { ICON_TYPE } from '../utils/constants';
 import { templateColumnElement } from './column';
 import { templateDeleteModalElement, templateEditModalElement } from './modal';
 import { Sections, fetchTodoList } from './fetch';
+import htmlElements from '../utils/htmlElement';
 
-const COLUMN_WRAP_CLASS: string = 'column-wrap';
+const COLUMN_WRAP_CLASS = {
+  columnWrap: 'column-wrap',
+  addButton: 'column-add-button',
+  icon: 'material-icons',
+};
+const COLUMN_ADD_BUTTON_TEXT: stirng = '컬럼 추가';
 
-export const columnWrapElement: HTMLElement = _q(`.${COLUMN_WRAP_CLASS}`);
+export const columnWrapElement: HTMLElement = _q(`.${COLUMN_WRAP_CLASS.columnWrap}`);
+
+const columnButton: string = htmlElements.div(COLUMN_WRAP_CLASS.addButton, htmlElements.icon(COLUMN_WRAP_CLASS.icon, ICON_TYPE.add), COLUMN_ADD_BUTTON_TEXT);
 
 export const initialRenderTodoList = async (): Promise<void> => {
   const sections = await fetchTodoList();
@@ -13,7 +22,7 @@ export const initialRenderTodoList = async (): Promise<void> => {
     allElements += templateColumnElement(eachSection);
     return allElements;
   }, '');
-  columnWrapElement.innerHTML = elementStr;
+  columnWrapElement.innerHTML = elementStr + columnButton;
 };
 
 export const renderDeleteModal = (): void => {
